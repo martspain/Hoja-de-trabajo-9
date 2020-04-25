10836f729ae94826c2bf504e13288c6644def92c
HanseLopez99 <halm29799@gmail.com>
2020-04-25 17:36:06 -0600

    Ligeros Cambios
    -Se agrego elegir mapping con factory en Driver.java
    -Cambios en comentarios de Controller.java

--- Controller.java
+++ Controller.java
@@ -5,7 +5,7 @@
  * Fecha de entrega: 27/04/2020
  * Clase Controller
  * Autores:
-	Hansel López	carné: 
+	Hansel López	carné: 19026 
 	Martin España 	carné: 19258
  * Última fecha de modificación: 20/04/2020
  * Versión: 1.0
--- Driver.java
+++ Driver.java
@@ -5,14 +5,38 @@
  * Fecha de entrega: 27/04/2020
  * Clase Driver
  * Autores:
-	Hansel López	carné: 
+	Hansel López	carné: 19026
 	Martin España 	carné: 19258
  * Última fecha de modificación: 20/04/2020
  * Versión: 1.0
 */
+import java.util.Scanner;
 
 public class Driver{
 	public static void main(String[] args){
-		//code
+		
+		Scanner sc = new Scanner(System.in);
+		Factory factory = new Factory();
+		Mapping<String, String> mapping = null;
+		String input = null;
+
+		//Imprimiendo opciones de mapping
+		System.out.println("________________________________________________________--");
+    	System.out.println("                 Hoja de Trabajo 6");
+    	System.out.println("________________________________________________________--\n");
+    	System.out.println("Que implementacion desea utilizar para traducir: \n");
+		System.out.println("Escribir: 'HashMap' o 'RedBlackTree'");
+		
+		while(mapping == null){
+			input = sc.nextLine();		
+			mapping = factory.getMapping(input);//Instanciando Mapping elegido por medio patron factory
+		}
+
+		if(mapping.isHashing()){
+			//Hashing code to translate here
+		}else{
+			//RedBlackTree code to translate here
+		}
+
 	}
 }
